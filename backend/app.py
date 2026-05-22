69846bd2d51e29406d8f499d287ea2ccdf318539
shruthichincholi992-oss <shruthichincholi992@gmail.com>
2026-05-22 12:43:18 +0530

    remove debug mode

--- backend/app.py
+++ backend/app.py
@@ -7,5 +7,6 @@ app = Flask(__name__)
 def home():
     return "Backend Running Successfully"
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+port = int(os.environ.get("PORT", 5000))
+
+app.run(host="0.0.0.0", port=port)
