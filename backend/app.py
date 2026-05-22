cc046e7a29765fa5010b916a62312b9d75cef47b
shruthichincholi992-oss <shruthichincholi992@gmail.com>
2026-05-22 12:24:02 +0530

    fixed render port

--- backend/app.py
+++ backend/app.py
@@ -1,9 +1,11 @@
 from flask import Flask
+import os
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Backend Running"
+    return "Backend Running Successfully"
 
-app.run(debug=True)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
