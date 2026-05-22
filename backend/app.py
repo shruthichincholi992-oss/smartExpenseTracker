5def8c0d27ce352cc531566aefc11dd9719f3e60
shruthichincholi992-oss <shruthichincholi992@gmail.com>
2026-05-22 12:33:34 +0530

    render final fix

--- backend/app.py
+++ backend/app.py
@@ -8,5 +8,4 @@ def home():
     return "Backend Running Successfully"
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
